ffe0b4841aed7dea308b101730748dff3b076f8b
Kartik <kartikgole77@gmail.com>
2019-02-26 17:09:38 -0600

    comm #3

--- Client.py
+++ Client.py
@@ -12,7 +12,7 @@ TCP_PORT_NUMBER = int(input("Enter Port Number : "))                  # For taki
 BUFFER_SIZE = 1024
 
 tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # Client socket object creation
-tcpsocket.connect((TCP_IP_ADDRESS, TCP_PORT_NUMBER))                     # Establishing a connection with the Server using the
+tcpsocket.connect((TCP_IP_ADDRESS, TCP_PORT_NUMBER))              # Establishing a connection with the Server using the
                                                                   # Ip Address and port number
 print("Connected to the Server")
 
@@ -24,19 +24,19 @@ print("The filename : ", file)
 print("Peer name : "+str(tcpsocket.getpeername()))
 
 tcpsocket.send(b'msg /'+file.encode())                         # Sending filename and requesting it from the server
-Starttime = time.time()                                            # Time of the request
+Starttime = time.time()                                        # Time of the request
 while True:                                                    # This While loop handles the incoming file
     content = tcpsocket.recv(BUFFER_SIZE)                      # Content check
     if not content:
         break                                                  # Break the loop if there is no content in the file
     print(content)
 Endtime = time.time()                                              # Record time when the file was received for RTT calculation
-rdtriptime = Endtime - Starttime                                       # Calculate the Round Trip Time (RTT)
+rdtriptime = Endtime - Starttime                                   # Calculate the Round Trip Time (RTT)
 
 print("\nRound Trip Time (RTT) : "+str(rdtriptime) + " Seconds ")   #Print calculated Round Trip Time
 print('Socket Family : '+str(tcpsocket.family))                     #Print Socket Family
 print('Socket Protocol : '+str(tcpsocket.proto))                    #Print the protocol name
 print('Socket Type : '+str(tcpsocket.type))                         #Print Socket Type
 print('Time out : '+str(tcpsocket.timeout))                         #Total time out
-tcpsocket.close()                                                  # Close the Client Socket
+tcpsocket.close()                                                   # Close the Client Socket
 print('\nThe Connection is closed')
